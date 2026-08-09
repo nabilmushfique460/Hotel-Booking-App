c4e61711afb5bfd0548832d0433a8974412fd67e
NABIL <nabilmushfique460@gmail.com>
2026-08-09 23:53:52 +0600

    Add securecreditcard class

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id":str})
 df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_cards_security = pandas.read_csv("card-security.csv", dtype=str)
 
 
 class Hotel:
     def __init__(self, id):
         self.hotel_id = hotel_id
         self.name = df.loc[df["id"] == hotel_id, "name"].squeeze()
+        self.city = df.loc[df["id"] == hotel_id, "city"].squeeze()
 
     def book(self):
         """Book a hotel by changing its availability to no"""
@@ -25,16 +27,20 @@ class Hotel:
 
 
 class ReservationTicket:
-    def __init__(self, customer_name, hotel_object):
+    def __init__(self, customer_name, hotel_object, booking_date):
         self.customer_name = customer_name
         self.hotel = hotel_object
+        self.booking_date = booking_date
 
     def generate(self):
         content = f"""
-        Thank you for your reservation!
         Here are your booking data:
         Name: {self.customer_name} 
         Hotel name: {self.hotel.name}
+        City: {self.hotel.city}
+        Booking date: {self.booking_date}
+        Pay by card.
+        Thank you for your reservation!
         """
         return content
 
@@ -50,17 +56,36 @@ class CreditCard:
             return False
 
 
+class SecureCreditcard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+df.index = df.index + 1
 print(df)
 hotel_id = input("Enter the hotel id: ")
 hotel = Hotel(hotel_id)
 
 if hotel.available():
-    credit_card = CreditCard(number="1234567890123456")
-    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(name ,hotel)
-        print(reservation_ticket.generate())
+    name = input("Enter your name: ")
+    name = name.title()
+    card_number = input("Enter your card number(demo: 1234567890123456): ")
+    card_expired_date = input("Enter your card expiration date(demo: 12/26): ")
+    card_holder_name = input("Enter your card holder name(demo: JOHN SMITH): ")
+    card_cvc_number = input("Enter your card CVC number(demo: 123): ")
+    credit_card = SecureCreditcard(number=card_number)
+    if credit_card.validate(expiration=card_expired_date, holder=card_holder_name, cvc=card_cvc_number):
+        password = input("Enter your password(demo: mypass): ")
+        if credit_card.authenticate(given_password=password):
+            book_date = input("Enter your booking date (demo: 2026-10-15): ")
+            hotel.book()
+            reservation_ticket = ReservationTicket(name ,hotel,book_date)
+            print(reservation_ticket.generate())
+        else:
+            print("Credit card authentication failed.")
     else:
         print("Sorry! There was a problem with your payment")
 else:

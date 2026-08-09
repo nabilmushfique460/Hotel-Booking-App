f4fb6f3ef52454c51623770dd62891e581ac4fe2
NABIL <nabilmushfique460@gmail.com>
2026-08-09 22:36:50 +0600

    Add creditcard class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id":str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -37,13 +38,30 @@ class ReservationTicket:
         """
         return content
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data ={"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_id = input("Enter the hotel id: ")
 hotel = Hotel(hotel_id)
+
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name ,hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(name ,hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("Sorry! There was a problem with your payment")
 else:
     print("Sorry! Hotel is not available")

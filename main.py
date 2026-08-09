2cd474e1367a7f297315b3ed457ff4f7039d6264
NABIL <nabilmushfique460@gmail.com>
2026-08-09 15:52:31 +0600

    Add methods to ReservationTicket class

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ df = pandas.read_csv("hotels.csv", dtype={"id":str})
 class Hotel:
     def __init__(self, id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == hotel_id, "name"].squeeze()
 
     def book(self):
         """Book a hotel by changing its availability to no"""
-        df.loc[df["id"] == hotel_id, "available"] = "no"
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
         df.to_csv("hotels.csv", index=False)
 
     def available(self):
         """Check if the hotel available"""
-        availability = df.loc[df["id"] == hotel_id, "available"].squeeze()
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
         if availability == "yes":
             return True
         else:
@@ -23,11 +24,17 @@ class Hotel:
 
 
 class ReservationTicket:
-    def __init__(self, customer_name, hotel):
-        pass
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        content = F"Name of the customer hotel"
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name} 
+        Hotel name: {self.hotel.name}
+        """
         return content
 
 print(df)
@@ -38,3 +45,5 @@ if hotel.available():
     name = input("Enter your name: ")
     reservation_ticket = ReservationTicket(name ,hotel)
     print(reservation_ticket.generate())
+else:
+    print("Sorry! Hotel is not available")
